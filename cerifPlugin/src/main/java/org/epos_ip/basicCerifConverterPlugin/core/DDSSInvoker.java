8144f90ed5381a0679b1ade0fed22ceb7fa5be1a
Valerio Vinciarelli <valerio.vinciarelli@ingv.it>
2019-04-11 16:08:12 +0200

    jar  with dependencies

--- cerifPlugin/src/main/java/org/epos_ip/basicCerifConverterPlugin/core/DDSSInvoker.java
+++ cerifPlugin/src/main/java/org/epos_ip/basicCerifConverterPlugin/core/DDSSInvoker.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
+import org.bson.Document;
 import org.epos_ip.basicCerifConverterPlugin.GenericMapper.JSONDDSS;
 import org.epos_ip.beans.DDSS;
 import org.epos_ip.beans.Distribution;
@@ -15,6 +16,8 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
 
 public class DDSSInvoker{
 	
@@ -60,6 +63,18 @@ public class DDSSInvoker{
 			Gson gson = new Gson();
 			JsonParser jsonParser = new JsonParser();
 
+			BasicDBObject ddssQueryList = new BasicDBObject();
+			FindIterable<Document> ddssCursorList = DatabaseConnection.getInstance().getDDSSTable().find(ddssQueryList);
+			ArrayList<String> ddssValue = new ArrayList<>();
+			ddssList.forEach(e -> ddssValue.add(e.getDdss()));
+			for (Document doc : ddssCursorList) {
+				if(!ddssValue.contains(doc.get("ddss").toString())) {
+					DDSS ddss1 = new DDSS();
+					ddss1.setDdss(doc.get("ddss").toString());
+					ddssList.add(ddss1);
+				}
+			}
+
 			System.out.println();
 
 			/**
@@ -84,7 +99,36 @@ public class DDSSInvoker{
 
 			ddssList.forEach(ds -> {
 				System.out.println(ds.toString());
-			
+				BasicDBObject ddssQuery = new BasicDBObject();
+				ddssQuery.put("ddss", ds.getDdss());
+				FindIterable<Document> ddssCursor = DatabaseConnection.getInstance().getDDSSTable().find(ddssQuery);
+				for (Document doc : ddssCursor) {
+					if(doc.get("ddss").toString().equals(ds.getDdss())) {
+						ds.setTitle(doc.get("name").toString());
+					}
+				}
+
+				ds.getDistributions().forEach(distr -> {
+					BasicDBObject whereQuery = new BasicDBObject();
+					whereQuery.put("distributionid", distr.getId());
+					FindIterable<Document> cursor = DatabaseConnection.getInstance().getTableDefaults().find(whereQuery);
+					for (Document doc : cursor) {
+						if(doc.get("distributionid").toString().equals(distr.getId())) {
+							distr.setDefaultURL(doc.get("defaultURL").toString());
+							if(doc.containsKey("defaultFormat")) distr.setDefaultFormat(doc.get("defaultFormat").toString());
+							else distr.setDefaultFormat(doc.get("originalFormat").toString());
+						}
+					}
+					try {
+						if(distr.getStartDate()!=null || distr.getEndDate()!=null) {
+							distr.setStartDate(Utils.convert(distr.getStartDate(),distr.getPattern(),Utils.EPOS_internal_format));
+							distr.setEndDate(Utils.convert(distr.getEndDate(),distr.getPattern(),Utils.EPOS_internal_format));
+						}
+					} catch (ParseException e1) {
+						e1.printStackTrace();
+					}
+
+				});
 
 			});
 			resultJson.getAsJsonObject().add("ddss", jsonParser.parse(gson.toJson(ddssList)).getAsJsonArray());
