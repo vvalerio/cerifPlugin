00056fc061c14a6ea087e72624d1f590cb5e9da4
Valerio Vinciarelli <valerio.vinciarelli@ingv.it>
2019-04-20 16:29:10 +0200

    up

--- cerifPlugin/src/main/java/org/epos_ip/basicCerifConverterPlugin/core/CerifPluginInvoker.java
+++ cerifPlugin/src/main/java/org/epos_ip/basicCerifConverterPlugin/core/CerifPluginInvoker.java
@@ -222,4 +222,174 @@ public class CerifPluginInvoker extends CallableJavaPlugin {
 		}
 		return Optional.of(resultJson.getAsString());
 	}
+	
+	protected static Optional<String> doTest(String payload) {
+		JsonObject resultJson = new JsonObject();
+		JsonObject payloadJson  =  gson.fromJson(payload, JsonObject.class);
+
+		JsonObject summary = null;
+		JsonArray keywords = null;
+		JsonArray domains = null;
+		JsonArray facets = null;
+		JsonArray ddss = null;
+		HashMap<String, HashSet<String>> keywordsComplex = new HashMap<>();
+		HashMap<String, HashSet<String>> organisationsComplex = new HashMap<>();
+		ArrayList<DDSS> ddssList = new ArrayList<DDSS>();
+		ArrayList<Distribution> distributionList = new ArrayList<Distribution>();
+		int counter = 0;
+
+		JsonArray results = new JsonArray();
+
+		for(Entry<String, JsonElement> entries : payloadJson.entrySet())
+		{
+			JsonArray result = (JsonArray) entries.getValue();
+			for(int i = 0; i<result.size(); i++)
+			{
+				counter++;
+				switch(entries.getKey().replaceAll("ResultSet_", "").split("_")[0])
+				{
+				case "person":
+					summary = new JsonObject();
+					JSONPerson.generate(results, result, i);
+					break;
+				case "organisation":
+					summary = new JsonObject();
+					JSONOrganisation.generate(results, result, i);
+					break;
+				case "webservice":
+					JSONWebservice.generate(results, result, i);
+					break;
+				case "domains":
+					domains = new JsonArray();
+					JSONDomains.generate(resultJson, results, domains, result, i);
+					break;
+				case "keywords":
+					keywords = new JsonArray();
+					JSONKeywords.generate(keywords, result, i);
+					break;
+				case "facets":
+					facets = new JsonArray();
+					keywords = new JsonArray();
+					if(entries.getKey().replaceAll("ResultSet_", "").split("_")[1].equals("keywords")) {
+						JSONFacetsKeywords.generate(keywordsComplex, result, i);
+					}
+					if(entries.getKey().replaceAll("ResultSet_", "").split("_")[1].equals("organisation")) {
+						JSONFacetsOrganisations.generate(organisationsComplex, result, i);
+					}
+					break;
+				case "ddss":
+					ddss = new JsonArray();
+					JSONDDSS.generate(ddssList, distributionList, result, i);
+					break;
+				}
+			}
+		}
+
+		if(facets!= null) {
+			JsonArray kw = new JsonArray();
+			keywordsComplex.entrySet().forEach(r -> {
+				JsonObject jsonObject = new JsonObject();
+				jsonObject.addProperty("name", r.getKey());
+				JsonArray ddssids = new JsonArray();
+				r.getValue().forEach(e -> ddssids.add(e));
+				jsonObject.add("ddss", ddssids);
+				kw.add(jsonObject);
+			});
+			JsonObject keywordsObject = new JsonObject();
+			keywordsObject.addProperty("name", "keywords");
+			keywordsObject.add("children", kw);
+			facets.add(keywordsObject);
+			JsonArray org = new JsonArray();
+			organisationsComplex.entrySet().forEach(r -> {
+				JsonObject jsonObject = new JsonObject();
+				jsonObject.addProperty("name", r.getKey());
+				JsonArray ddssids = new JsonArray();
+				r.getValue().forEach(e -> ddssids.add(e));
+				jsonObject.add("ddss", ddssids);
+				org.add(jsonObject);
+			});
+			JsonObject organisationsObject = new JsonObject();
+			organisationsObject.addProperty("name", "organisations");
+			organisationsObject.add("children", org);
+			facets.add(organisationsObject);
+			resultJson.add("facets", facets);
+		}
+		if(ddss!= null) {
+			Gson gson = new Gson();
+			JsonParser jsonParser = new JsonParser();
+
+			BasicDBObject ddssQueryList = new BasicDBObject();
+			FindIterable<Document> ddssCursorList = DatabaseConnection.getInstance().getDDSSTable().find(ddssQueryList);
+			ArrayList<String> ddssValue = new ArrayList<>();
+			ddssList.forEach(e -> ddssValue.add(e.getDdss()));
+			for (Document doc : ddssCursorList) {
+				if(!ddssValue.contains(doc.get("ddss").toString())) {
+					DDSS ddss1 = new DDSS();
+					ddss1.setDdss(doc.get("ddss").toString());
+					ddssList.add(ddss1);
+				}
+			}
+
+			System.out.println();
+
+			/**
+			 * CHECK FOR EMPTY DDSSs
+			 */
+			for(Iterator<DDSS> iterator = ddssList.iterator(); iterator.hasNext();) {
+				DDSS ddssNext = iterator.next();
+				if(ddssNext.getDdss()==null) 
+					iterator.remove();
+			}
+
+
+			ddssList.forEach(ds -> {
+				//System.out.println("productlist: "+ds.getProductid().toString());
+				distributionList.forEach(dist -> {
+					System.out.println(dist.getProductid().toString());
+					if(ds.getProductid().contains(dist.getProductid())) {
+						ds.getDistributions().add(dist);
+					}
+				});
+			});
+
+			ddssList.forEach(ds -> {
+				System.out.println(ds.toString());
+				BasicDBObject ddssQuery = new BasicDBObject();
+				ddssQuery.put("ddss", ds.getDdss());
+				FindIterable<Document> ddssCursor = DatabaseConnection.getInstance().getDDSSTable().find(ddssQuery);
+				for (Document doc : ddssCursor) {
+					if(doc.get("ddss").toString().equals(ds.getDdss())) {
+						ds.setTitle(doc.get("name").toString());
+					}
+				}
+
+				ds.getDistributions().forEach(distr -> {
+					BasicDBObject whereQuery = new BasicDBObject();
+					whereQuery.put("distributionid", distr.getId());
+					FindIterable<Document> cursor = DatabaseConnection.getInstance().getTableDefaults().find(whereQuery);
+					for (Document doc : cursor) {
+						if(doc.get("distributionid").toString().equals(distr.getId())) {
+							distr.setDefaultURL(doc.get("defaultURL").toString());
+							if(doc.containsKey("defaultFormat")) distr.setDefaultFormat(doc.get("defaultFormat").toString());
+							else distr.setDefaultFormat(doc.get("originalFormat").toString());
+						}
+					}
+					try {
+						if(distr.getStartDate()!=null || distr.getEndDate()!=null) {
+							distr.setStartDate(Utils.convert(distr.getStartDate(),distr.getPattern(),Utils.EPOS_internal_format));
+							distr.setEndDate(Utils.convert(distr.getEndDate(),distr.getPattern(),Utils.EPOS_internal_format));
+						}
+					} catch (ParseException e1) {
+						e1.printStackTrace();
+					}
+
+				});
+
+			});
+			resultJson.getAsJsonObject().add("ddss", jsonParser.parse(gson.toJson(ddssList)).getAsJsonArray());
+
+
+		}
+		return Optional.of(resultJson.toString());
+	}
 }
