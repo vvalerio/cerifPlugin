e5b4dd29a8a3010f1b072a39040c611a6ef2f033
Valerio Vinciarelli <valerio.vinciarelli@ingv.it>
2019-04-23 10:12:57 +0200

    up10

--- cerifPlugin/src/main/java/org/epos_ip/basicCerifConverterPlugin/GenericMapper/SpatialInformation.java
+++ cerifPlugin/src/main/java/org/epos_ip/basicCerifConverterPlugin/GenericMapper/SpatialInformation.java
@@ -4,10 +4,10 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 public class SpatialInformation {
-	
+
 	public static void main(String[] args) {
 		SpatialInformation.doSpatial("POLYGON((-34 74,45 74,45 33,-34 33,-34 74))");
-		SpatialInformation.doSpatial("POLYGON(7.86 48.93, 7.87 48.94)");
+		SpatialInformation.doSpatial("POLYGON(7.86, 48.93, 7.87, 48.94)");
 	}
 
 	public static JsonObject doSpatial(String spatial)
@@ -17,24 +17,26 @@ public class SpatialInformation {
 		wkid.addProperty("wkid", 4326);
 		spatialReturn.add("spatialReference", wkid);
 		spatial = spatial.replaceAll("POLYGON", "").replaceAll("POINT","").replaceAll("\\)","").replaceAll("\\(","");
-		
+
 		System.out.println(spatial);
-		
+
 		String[] points = spatial.split(",");
-		
+
 		JsonArray path = new JsonArray();
-		for(String point : points) {
-			String[] latlon  = point.trim().split(" ");
-			JsonArray points1 = new JsonArray();
-			points1.add(Double.parseDouble(latlon[0]));
-			points1.add(Double.parseDouble(latlon[1]));
-			path.add(points1);
-		}
-		
+		try {
+			for(String point : points) {
+				String[] latlon  = point.trim().split(" ");
+				JsonArray points1 = new JsonArray();
+				points1.add(Double.parseDouble(latlon[0]));
+				points1.add(Double.parseDouble(latlon[1]));
+				path.add(points1);
+			}
+		}catch(Exception e) {}
+
 		spatialReturn.add("path", path);
-		
+
 		System.out.println(spatialReturn.toString());
-		
+
 		return spatialReturn;
 	}
 }
